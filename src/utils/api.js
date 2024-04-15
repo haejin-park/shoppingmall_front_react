caf0f7cfee1c0cdcd53c240775c6c69d90bf7120
haejin <luckyhaejin1126@gmail.com>
2024-04-15 21:31:34 +0900

    배포 준비(uuid버전 업그레이드, pm2, cross-env설치, Url설정)

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,15 +1,20 @@
 import axios from "axios";
-const LOCAL_BACKEND = process.env.REACT_APP_LOCAL_BACKEND;
-// const PROD_BACKEND = process.env.REACT_APP_PROD_BACKEND;
-// const BACKEND_PROXY = process.env.REACT_APP_BACKEND_PROXY;
+
 const token = sessionStorage.getItem("token");
+
+let backURL = process.env.NODE_ENV === 'production' 
+?  `${process.env.REACT_APP_BACK_PROD_URL}/api`
+: `${process.env.REACT_APP_BACK_DEV_URL}/api`
+
 const api = axios.create({
-  baseURL: LOCAL_BACKEND,
+  baseURL: backURL,
+  withCredentials : true,
   headers: {
     "Content-Type": "application/json",
     "authorization": token? `Bearer ${token}` : undefined
   },
 });
+
 api.interceptors.request.use(
   (request) => {
     return request;
